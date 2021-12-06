b3f7a9a5364ae22f32eb4640b55757fff067386c
souma007 <ismaila.souma.ndong@gmail.com>
2021-12-06 12:19:00 -0500

    SAMASON

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const express = require("express");
+
+const PORT = 8000;
+
+express()
+  .use(express.json())
+
+  .get("/hello", (req, res) => {
+    res.status(200).json({ hi: "hi" });
+  })
+
+  .listen(PORT, function () {
+    console.info("🌍 Listening on port " + PORT);
+  });
