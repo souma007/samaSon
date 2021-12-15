4712e865563ee83afbef115cee1164028d369efe
souma007 <ismaila.souma.ndong@gmail.com>
2021-12-15 16:09:39 -0500

    Functionality Done

--- client/src/PlaylistDetails.js
+++ client/src/PlaylistDetails.js
@@ -4,6 +4,9 @@ import { UseAuthContext } from "./UseAuthProvider";
 import { useParams } from "react-router-dom";
 import { useEffect, useContext } from "react";
 import SpotifyWebApi from "spotify-web-api-node";
+import { useState } from "react";
+import TrackPlaylistResult from "./TrackPlaylistResult";
+import Player from "./Player";
 
 const spotifyApi = new SpotifyWebApi({
   clientId: "ea01f9e43e4746eca67543f410ac56d7",
@@ -12,23 +15,16 @@ const spotifyApi = new SpotifyWebApi({
 const PlaylistDetails = () => {
   const { accessToken } = useContext(UseAuthContext);
   console.log(accessToken);
+  const [playlist, setPlaylist] = useState(null);
+  const [playingTrack, setPlayingTrack] = useState(null);
+  const [search, setSearch] = useState(null);
+
+  const chooseTrack = (track) => {
+    setPlayingTrack(track);
+    setSearch("");
+  };
 
   const { playlistId } = useParams();
-
-  // useEffect(() => {
-  //   if (!accessToken) return;
-  //   spotifyApi.getMe().then(
-  //     function (data) {
-  //       console.log(data);
-  //       console.log("Some information about the authenticated user", data.body);
-  //       const user_Id = data.body.id;
-  //       setUserId(userId);
-  //     },
-  //     function (err) {
-  //       console.log("Something went wrong!", err);
-  //     }
-  //   );
-  // }, [accessToken]);
 
   useEffect(() => {
     if (!accessToken) return;
@@ -37,6 +33,10 @@ const PlaylistDetails = () => {
     spotifyApi.getPlaylist(`${playlistId}`).then(
       function (data) {
         console.log("Some information about this playlist", data.body);
+        const { images, name, owner, tracks, uri } = data.body;
+        const playlistInfo = { images, name, owner, tracks, uri };
+        console.log(playlistInfo);
+        setPlaylist(playlistInfo);
       },
       function (err) {
         console.log("Something went wrong!", err);
@@ -44,9 +44,87 @@ const PlaylistDetails = () => {
     );
   }, [accessToken]);
 
-  return <Main></Main>;
+  console.log(playlist);
+
+  return (
+    <Main>
+      {playlist && (
+        <>
+          <Wrapper>
+            <Art>
+              <Image
+                src={playlist.images[0].url}
+                style={{ height: "500px", width: "500px" }}
+              />
+              <Creator>
+                <Title>{playlist.name}</Title>
+                <Name>{playlist.owner.display_name}</Name>
+              </Creator>
+            </Art>
+
+            <Tracklist>
+              {playlist.tracks.items.map((item) => {
+                return (
+                  <TrackPlaylistResult
+                    track={item.track}
+                    chooseTrack={chooseTrack}
+                  />
+                );
+              })}
+            </Tracklist>
+          </Wrapper>
+
+          <Record>
+            <Player
+              accessToken={accessToken}
+              trackUri={playingTrack && playingTrack.uri}
+            />
+          </Record>
+        </>
+      )}
+    </Main>
+  );
 };
 
 export default PlaylistDetails;
 
 const Main = styled.div``;
+
+const Wrapper = styled.div`
+  display: flex;
+  margin: 100px;
+  padding: 50px;
+`;
+
+const Art = styled.div`
+  border: red solid;
+`;
+
+const Image = styled.img``;
+
+const Name = styled.div`
+  color: white;
+`;
+
+const Tracklist = styled.div`
+  border: green solid;
+  margin: 100px;
+  padding: 50px;
+`;
+
+const Creator = styled.div`
+  border: blue solid;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.div`
+  color: white;
+`;
+
+const Track = styled.div``;
+
+const Record = styled.div``;
---
+++ client/src/TrackPlaylistResult.js
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from "styled-components";
+import Background from "./Background";
+
+const TrackPlaylistResult = ({ track, chooseTrack }) => {
+  const handlePlay = () => {
+    chooseTrack(track);
+  };
+
+  console.log(track.album.images[0].url);
+
+  return (
+    <Main onClick={handlePlay}>
+      <Cover src={track.album.images[0].url} />
+      <InfoTrack>
+        <Title>{track.album.name}</Title>
+        <Name>{track.artists[0].name}</Name>
+      </InfoTrack>
+    </Main>
+  );
+};
+
+export default TrackPlaylistResult;
+
+const Main = styled.div`
+  background-color: black;
+  margin: 25px;
+  display: flex;
+`;
+
+const Cover = styled.img`
+  width: 100px;
+  height: 100px;
+`;
+
+const InfoTrack = styled.div`
+  color: white
+  background-color: red;
+  border: red solid;
+  width: 300px;
+  padding: 25px
+`;
+
+const Title = styled.div`
+  color: white;
+`;
+
+const Name = styled.div`
+  color: white;
+`;
